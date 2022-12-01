9a0036352ce2d7a24e38f88ddd6da026ae61b2d5
marinaioana <115020895+marinaioana@users.noreply.github.com>
2022-12-01 18:05:13 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+#include "primaLibreria.h"
+#include "iostream"
+using namespace std;
+int numeroprimo ( int a, int b){
+    int risultato = a/b;
+        if ( risultato == 1){
+            cout << " vero" << endl;
+
+
+    } else {
+            cout << " falso " << endl;
+        }
+
+    return 0;
 
